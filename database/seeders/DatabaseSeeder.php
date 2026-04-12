6656bfdbcfda7775b9726d7f78ea3a3bb9a9f00d
Nyne <96626242+taugk@users.noreply.github.com>
2026-04-12 13:36:13 +0700

    fix db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,29 +16,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // 1. SEED USERS (Admin, Manager, Kasir)
-        User::factory()->create([
+       
+        User::create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
             'role' => 'admin',
             'password' => bcrypt('password'),
         ]);
 
-        User::factory()->create([
+        User::create([
             'name' => 'Manager',
             'email' => 'manager@example.com',
             'role' => 'manager',
             'password' => bcrypt('password'),
         ]);
 
-        User::factory()->create([
+        User::create([
             'name' => 'Kasir',
             'email' => 'kasir@example.com',
             'role' => 'kasir',
             'password' => bcrypt('password'),
         ]);
-
-        // 2. SEED PRODUK & KATEGORI
         
 
        
